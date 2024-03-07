bb2cbcc9fb830c12c489a6bdf26391be4fc9b9eb
otocmpa <41562938+OTCompa@users.noreply.github.com>
2024-03-07 00:36:12 -0600

    Create ucobmitty.js

---
+++ commands/utility/ucobmitty.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ucobmitty')
+		.setDescription('Responds with the COB Enjoyers certified COBMitty'),
+	async execute(interaction) {
+		await interaction.reply('COBMitty - <https://tinyurl.com/ucobmitty> \n<https://imgur.com/a/arELu8e> (Imgur album)');
+	},
+};
