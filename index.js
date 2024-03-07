41a230f0b58d504ba7a2e34109232ab027775bd7
otocmpa <41562938+OTCompa@users.noreply.github.com>
2024-03-07 00:43:51 -0600

    i'm dumb

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.once(Events.ClientReady, readyClient => {
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
 
-client.user.setActivity('activity');
+client.user.setActivity('UCoB');
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
