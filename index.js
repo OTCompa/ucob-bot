6348817a7ddbed5ff0e8460c059459c5cb56c30a
otocmpa <41562938+OTCompa@users.noreply.github.com>
2024-03-07 00:46:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.once(Events.ClientReady, readyClient => {
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
 
-client.user.setActivity('UCoB');
+client.user.setPresence({ activities: [{ name: 'UCoB' }], status: 'online' });
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
