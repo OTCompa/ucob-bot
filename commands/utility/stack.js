fd40399d67f973cce3db354909e037142980b0da
otocmpa <41562938+OTCompa@users.noreply.github.com>
2025-09-27 07:21:27 -0500

    grammar

--- commands/utility/stack.js
+++ commands/utility/stack.js
@@ -8,7 +8,7 @@ module.exports = {
     await interaction.reply(
       `
 # Please just stack in every Fireball after the first one, no matter what role you are!!!
-A lot of people try to stay out of the 2nd Fireball stack (roughly 90s in with 2 Hatches and a random Liquid Hell).
+A lot of people try to stay out of the 2nd Fireball stack (roughly 90s in with 2 Hatches and a set of random Liquid Hells).
 This has the potential to cause unnecessary deaths for no good reason, as you don't need LB3 to survive Nael -> Bahamut transition.
 https://www.youtube.com/watch?v=tZ9negg9TWk
       `
