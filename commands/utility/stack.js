cf6c1345296da076d4a6c2d65ee76f6a037c24d2
otocmpa <41562938+OTCompa@users.noreply.github.com>
2025-09-27 07:15:26 -0500

    fix indentation

--- commands/utility/stack.js
+++ commands/utility/stack.js
@@ -7,10 +7,10 @@ module.exports = {
   async execute(interaction) {
     await interaction.reply(
       `
-        # Please just stack in every Fireball after the first one, no matter what role you are!!!
-        A lot of people try to stay out of the 2nd Fireball stack (roughly 90s in with 2 Hatches and a random Liquid Hell).
-        This has the potential to cause unnecessary deaths for no good reason, as you don't need LB3 to survive Nael -> Bahamut transition.
-        https://www.youtube.com/watch?v=tZ9negg9TWk
+# Please just stack in every Fireball after the first one, no matter what role you are!!!
+A lot of people try to stay out of the 2nd Fireball stack (roughly 90s in with 2 Hatches and a random Liquid Hell).
+This has the potential to cause unnecessary deaths for no good reason, as you don't need LB3 to survive Nael -> Bahamut transition.
+https://www.youtube.com/watch?v=tZ9negg9TWk
       `
     );
   },
